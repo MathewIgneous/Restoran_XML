62732a65f8ac0b997106f51229ade9b907e67299
MathewIgneous <mateo.cahun@outlook.com>
2017-06-05 13:47:40 +0200

    Added menu.php file

---
+++ Menu.php
@@ -0,0 +1,50 @@
+﻿<?php $image = "images/Pozadina02.jpeg"; require_once('header.php'); 
+
+	$menu = new SimpleXMLElement(file_get_contents('menu.xml'));
+?>
+	<main>
+		<div class="center_wrapper">
+			<table class="menu">
+				<tr>
+					<th>Predjelo</th>
+					<th>Cijena</th>
+				</tr>
+				<?php generate_row($menu, "Predjelo") ?>
+				<tr>
+					<th>Glavno jelo</th>
+				</tr>
+				<?php generate_row($menu, "Glavno jelo") ?>
+				<tr>
+					<th>Prilog</th>
+				</tr>
+				<?php generate_row($menu, "Prilog") ?>
+				<tr>
+					<th>Desert</th>
+				</tr>
+				<?php generate_row($menu, "Desert") ?>
+			</table>
+		</div>
+	</main>
+<?php require_once('footer.php');
+
+	function generate_row($menu, $vrsta)
+	{
+		foreach ($menu->Jelo as $jelo) 
+		{
+			if (((string)$jelo['vrsta']) == $vrsta)
+			{
+				$naziv = $jelo->Naziv;
+				$cijena = $jelo->Cijena;
+
+				echo <<<_END
+					<tr>
+						<td>$naziv</td>
+						<td>$cijena kn</td>
+					</tr>
+_END;
+			}
+		}
+	}
+ ?>
+
+	
