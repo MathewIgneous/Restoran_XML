3dd755da2a01645b4de7e46ffbbea2debb2498df
MathewIgneous <mateo.cahun@outlook.com>
2017-06-05 12:15:22 +0200

    I just added index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+﻿<?php $image = "images/Pozadina01.jpg"; require_once('header.php') ?>
+	<main>
+		<section class="welcome_message">
+			<h1>Welcome</h1>
+		</section>
+	</main>
+<?php require_once('footer.php') ?>
